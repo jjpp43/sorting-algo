f710eaaec8b28aba8b1e9687d9434483494a2853
jjpp43 <junnapark@gmail.com>
2022-04-27 12:18:22 +0000

    minor changes + performance memo

--- package/index.js
+++ package/index.js
@@ -19,13 +19,21 @@ Cocktail Sort
 Strand Sort
 */
 
+/*
+How to measure performance time
+    const t0 = performance.now();
+    bubbleSort(nums);
+    const t1 = performance.now()
+    console.log(`It took ${t1 -t0} milliseconds`)
+*/
+
 function swap(array, xindex, yindex) {
     let temp = array[xindex];
     array[xindex] = array[yindex];
     array[yindex] = temp;
 }
 
-//------------Selection Sort------------//
+//------------Selection Sort------------O//
 function selectionSort(array) {
     let min_index;
     for(let i = 0; i < array.length - 1; i++) {
@@ -40,7 +48,7 @@ function selectionSort(array) {
     return array
 }
 
-//------------Bubble Sort------------//
+//------------Bubble Sort------------O//
 function bubbleSort(array) {
     for(let i = 0; i < array.length; i++) {
         for(let j = 0; j < array.length - i - 1; j++) {
@@ -55,14 +63,14 @@ function bubbleSort(array) {
 //------------Insertion Sort------------//
 function insertionSort(array) {
     for(let i = 1; i < array.length; i++) {
-        let current = arr[i]
+        let current = array[i]
         let j = i - 1
         //Through a while loop, we go through the sorted array and shift elements to the right, opening up space for current element to be inserted
-        while(j >= 0 && arr[j] > current) {
-            arr[j+1] = arr[j]
+        while(j >= 0 && array[j] > current) {
+            array[j+1] = array[j]
             j = j-1
         }
-        arr[j+1] = current
+        array[j+1] = current
     }
     return array
 }
@@ -70,9 +78,5 @@ function insertionSort(array) {
 //------------Merge Sort------------//
 
 module.exports = {
-    swap: function(array, xindex, yindex) {
-        let temp = array[xindex];
-        array[xindex] = array[yindex];
-        array[yindex] = temp;
-    }
+
 }
